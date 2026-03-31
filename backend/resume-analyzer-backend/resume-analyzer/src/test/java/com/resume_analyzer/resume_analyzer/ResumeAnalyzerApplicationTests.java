724fd4c3a91dd8f99d73caaf33c45665662fc4e9
Uday Shinde <ushinde7027@gmail.com>
2026-03-31 11:36:10 +0530

    Setup Spring Boot backend project

---
+++ backend/resume-analyzer-backend/resume-analyzer/src/test/java/com/resume_analyzer/resume_analyzer/ResumeAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume_analyzer.resume_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
